e51150c757c77831d2b69d0f30409e53501946d2
Rodrigo Ehlers <rodrigoehlers@outlook.com>
2021-05-13 22:14:07 +0200

    initial serialization utils

---
+++ src/serialization.ts
@@ -0,0 +1,8 @@
+export const getAssignmentFromJSON = (json: string): NCA.Assignment => {
+  // TODO: Validation and error handling.
+  return JSON.parse(json) as NCA.Assignment;
+};
+
+export const getJSONFromAssignment = (assignment: NCA.Assignment): string => {
+  return JSON.stringify(assignment, null, 2);
+};
